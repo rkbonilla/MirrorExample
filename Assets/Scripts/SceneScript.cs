fb195f11fd4061b56a0d9e94f579dfa923e1a029
Bonilla-Reyes <rbonillareyes@email.neit.edu>
2023-02-23 15:06:20 -0500

    Initial Commit

---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,52 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace QuickStart
+{
+    public class SceneScript : NetworkBehaviour
+    {
+        public TMP_Text canvasStatusText;
+        public PlayerScript playerScript;
+
+        public SceneReference sceneReference;
+
+        public TMP_Text canvasAmmoText;
+
+        [SyncVar(hook = nameof(OnStatusTextChanged))]
+        public string statusText;
+
+        void OnStatusTextChanged(string _Old, string _New)
+        {
+            //called from sync var hook, to update info on screen for all players
+            canvasStatusText.text = statusText;
+        }
+
+        public void ButtonSendMessage()
+        {
+            if (playerScript != null)
+                playerScript.CmdSendPlayerMessage();
+        }
+
+        public void ButtonChangeScene()
+        {
+            if (isServer)
+            {
+                Scene scene = SceneManager.GetActiveScene();
+                if (scene.name == "MyScene")
+                    NetworkManager.singleton.ServerChangeScene("MyOtherScene");
+                else
+                    NetworkManager.singleton.ServerChangeScene("MyScene");
+            }
+            else
+                Debug.Log("You are not Host.");
+        }
+
+        public void UIAmmo(int _value)
+        {
+            canvasAmmoText.text = "Ammo: " + _value;
+        }
+    }
+}
